be2fd1db9307be3e68df01696a8c0a989ecd8614
Hariharan <harimaniam@gmail.com>
2024-02-07 23:56:28 +0000

    HH-04-06: Understanding the dependency Arrys.

--- Exercise Files/Ch04/04_01/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_01/start/react-app/src/App.js
@@ -7,15 +7,23 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
 
   useEffect(() => {
-    console.log(`It's ${emotion} right now`);
+    console.log(`It's ${emotion} around here`);
   }, [emotion])
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here`);
+  }, [secondary])
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
       <button onClick={() => setEmotion("sad")}>Sad</button>
       <button onClick={() => setEmotion("excited")}>Excited</button>
+
+      <h1>Current secondary emotion is {secondary}</h1>
+      <button onClick={() => setSecondary("grateful")}>Grate Full</button>
     </div>
   );
 }
